0dea057fba3c819b0a0b4c2b6305b31cf87d687a
Matthew Wang <matt8p@Matthews-MacBook-Pro.local>
2025-10-20 21:27:57 -0700

    t

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = main;
-function main() {
-    console.log('Hello, world!');
-}
-if (require.main === module) {
-    main();
-}
